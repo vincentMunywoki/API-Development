1d8f468c40b1fc6fcf738716641f3e0c08caf79d
Vincent Munywoki <vincentmunywoki12@gmail.com>
2025-11-21 20:28:31 +0300

    created Validation folder and user.js file for schema

--- validations/user.js
+++ validations/user.js
@@ -1,1 +1,9 @@
-.
+const Joi = require ('joi');
+
+const createUserSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password:Joi.string().domain(6).required(),
+    name: Joi.string(50).optional()
+});
+
+module.exports = { createUserSchema };
