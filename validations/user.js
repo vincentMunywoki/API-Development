f5329d5a3182641d624b938aa1b3ae92fe3095c0
Vincent Munywoki <vincentmunywoki12@gmail.com>
2025-11-22 03:16:50 +0300

    Updated validations/user.js Add Custom Validation Logic
    to ensure passwords have a special character, or check if email is lowercase.

--- validations/user.js
+++ validations/user.js
@@ -1,9 +1,16 @@
 const Joi = require ('joi');
 
 const createUserSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password:Joi.string().domain(6).required(),
+    email: Joi.string().email().lowercase().required().custom((value, helpers) => {
+        if (value.endsWith('@example.com')) {
+            return helpers.error('any.invalid'); // Custom:Ban example.com domains
+        }
+        return value;
+    }, 'custom email validation'),
+    password:Joi.string().min(6).required().pattern(new RegExp('[@#$%^&+=]')), // Custom: Must have special char
     name: Joi.string(50).optional()
+}) .messages ({
+    'string.pattern.base': 'Password must contain at least one special character (@#$%^&+=)'
 });
 
 module.exports = { createUserSchema };
