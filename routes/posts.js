10d6bc0d285b2117cd27d854fa228f443803e2d1
Vincent Munywoki <vincentmunywoki12@gmail.com>
2025-11-24 15:04:40 +0300

    created posts.js file in routes folder

---
+++ routes/posts.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const validate = require('../middlewares/validate');
+const { createPostSchema } = require('../controllers/posts'); // Schema is in controller for simplicity
+const { createPost, getPosts, getPostById, updatePost, deletePost } = require('../controllers/posts');
+
+const router = express.Router();
+
+router.post('/', validate(createPostSchema), createPost);
+router.get('/', getPosts);
+router.get('/:id', getPostById);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.post('/:id/tags', addTagsToPost);
+
+module.exports = router;
