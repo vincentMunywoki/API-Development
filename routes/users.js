6e88347522481d4f0d01a9c3af4e9f1ed49f5470
Vincent Munywoki <vincentmunywoki12@gmail.com>
2025-11-24 14:42:47 +0300

    feat(users): implement full user routes with validation, serialization, and profile integration

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,58 @@
+// const express = require('express');
+// const { createUserSchema } = require('../validations/user');
+// const validate = require('../middlewares/validate');
+// const { checkUniqueEmail } = require('../middlewares/customValidate');
+// const { createUser, getUsers, getUserById, updateUser, deleteUser } = require('../controllers/users');
+// const { User, Profile } = require('../models');
+// const { userSerializer, userWithProfileSerializer } = require('../serializers/user');
+// const bcrypt = require('bcrypt');
+
+// const router = express.Router();
+
+// // Create User (with validation)
+// router.post('/', validate(createUserSchema), checkUniqueEmail, async (req, res) => {
+//     try {
+//         req.body.password = await bcrypt.hash(req.body.password, 10); // Hash password
+//         const user = await User.create(req.body);
+//         res.status(201).json(userSerializer(user));
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Server error' });
+//     }
+// });
+
+// // Get all users
+// router.get('/', async (req, res) => {
+//     try {
+//         const users = await User.findAll({ include: Profile });
+//         res.json(users.map(userWithProfileSerializer));
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Server error' });
+//     }
+// });
+
+
+// // Get User with Profile
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const user = await User.findByPk(req.params.id, { include: Profile });
+//         if (!user) return res.status(404).json({ error: 'User not found' });
+//         res.json(userWithProfileSerializer(user));
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Server error' });
+//     }
+// });
+
+// module.exports = router;
+
+
 const express = require('express');
 const { createUserSchema } = require('../validations/user');
 const validate = require('../middlewares/validate');
 const { checkUniqueEmail } = require('../middlewares/customValidate');
+const { createUser, getUsers, getUserById, updateUser, deleteUser } = require('../controllers/users');
 const { User, Profile } = require('../models');
 const { userSerializer, userWithProfileSerializer } = require('../serializers/user');
 const bcrypt = require('bcrypt');
